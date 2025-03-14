187582a507e44fbc406a0c3bd454dec93864ece4
manug859 <manu.gomez859@gmail.com>
2025-03-14 00:49:24 -0300

    resuelto bounds.c

--- lab00/bounds.c
+++ lab00/bounds.c
@@ -1,3 +1,7 @@
+/*
+$ gcc -Wall -Werror -Wextra -pedantic -std=c99 bounds.c -o bounds
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
@@ -13,24 +17,43 @@ struct bound_data {
 
 struct bound_data check_bound(int value, int arr[], unsigned int length) {
     struct bound_data res;
-    //
-    // TODO: COMPLETAR
-    //
+    res.exists = false;
+    res.is_upperbound = false;
+    res.is_lowerbound = false;
+    res.where = 0;
+
+    for(unsigned int i = 0; i < length; i++)
+    {
+        res.is_upperbound = res.is_upperbound || (value >= arr[i]);
+        res.is_lowerbound = res.is_lowerbound || (value < arr[i]);
+        res.exists = res.exists || (value == arr[i]);
+        if (value == arr[i])
+        {
+            res.where = i;
+        }
+    }
+    
     return res;
 }
 
 int main(void) {
-    int a[ARRAY_SIZE] = {0, -1, 9, 4};
-    int value=9;
-    //
-    // TODO: COMPLETAR - Pedir entrada al usuario
-    //
+    int a[ARRAY_SIZE];
+
+    int value; 
+    printf("Ingrese el valor a evaluar : ");
+    scanf("%d", &value);
+
+    for(int i = 0; i<ARRAY_SIZE; i++)
+    {
+        printf("Ingrese un valor para la posicion a[%d] : ", i);
+        scanf("%d", &a[i]);
+    }
+
     struct bound_data result = check_bound(value, a, ARRAY_SIZE);
 
-    // TODO: REEMPLAZAR LO SIGUIENTE LUEGO POR LA SALIDA QUE PIDE EL ENUNCIADO
     printf("%d", result.is_upperbound); // Imprime 1
     printf("%d", result.is_lowerbound); // Imprime 0
-    printf("%u", result.exists);        // Imprime 1
+    printf("%d", result.exists);        // Imprime 1
     printf("%u", result.where);         // Imprime 2
 
     return EXIT_SUCCESS;
