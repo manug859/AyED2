77588774b7c8cdc2945783378e2b3b3e3795c428
manug859 <manu.gomez859@gmail.com>
2025-03-22 00:41:08 -0300

    Implementar la función de inserción en el algoritmo de ordenamiento por inserción en sort.c

--- lab01/ej1/sort.c
+++ lab01/ej1/sort.c
@@ -8,7 +8,12 @@
 
 
 static void insert(int a[], unsigned int i, unsigned int length) {
-    /* needs implementation */
+    assert(i<length);
+    unsigned int j = i;
+        while(j > 0 && goes_before(a[j], a[j-1])) {
+            swap(a, j-1, j);
+            j--;
+        }
 }
 
 void insertion_sort(int a[], unsigned int length) {
