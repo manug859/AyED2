2b6d2b1301d7932ead3975a577a5ba6ed6f5a110
manug859 <manuel.agustin.gomez@mi.unc.edu.ar>
2025-03-26 19:07:24 -0300

    Parte C: chequeo de invariante implementada.

--- lab01/ej1/sort.c
+++ lab01/ej1/sort.c
@@ -8,17 +8,20 @@
 
 
 static void insert(int a[], unsigned int i, unsigned int length) {
+    
     assert(i<length);
     unsigned int j = i;
-        while(j > 0 && goes_before(a[j], a[j-1])) {
-            swap(a, j-1, j);
-            j--;
-            array_dump(a, length);
-        }
+
+    while(j > 0 && goes_before(a[j], a[j-1])) {
+        swap(a, j-1, j);
+        j--;
+        array_dump(a, length);
+        }       
 }
 
 void insertion_sort(int a[], unsigned int length) {
     for (unsigned int i = 1; i < length; ++i) {
         insert(a, i, length);
+        assert(array_is_sorted(a, i));
     }
 }
