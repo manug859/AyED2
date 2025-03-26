d1ecc29619d4d8135165ef9f9faba4d987198cfd
manug859 <manuel.agustin.gomez@mi.unc.edu.ar>
2025-03-26 19:51:09 -0300

    Implementar el algoritmo de ordenamiento rápido en sort.c y actualizar main.c para utilizar quick_sort.

--- lab01/ej2/sort.c
+++ lab01/ej2/sort.c
@@ -8,12 +8,13 @@
 
 
 static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
- /* needs implementation */
 
-    /* no implementes partition, ya está implementado en sort_helpers.o
-       (no se puede leer, pero en sort_helpers.h vas a encontrar información
-        para saber cómo usarlo)
-    */
+        if(izq > der) {
+            partition(a, izq, der);
+        }
+
+    
+
 }
 
 void quick_sort(int a[], unsigned int length) {
