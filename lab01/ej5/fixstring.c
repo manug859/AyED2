404e19a04701a4a39d9b7b909c0fe70b0c2207dc
manug859 <manu.gomez859@gmail.com>
2025-04-03 16:07:13 -0300

    Agregar archivo de entrada de ejemplo y completar funciones de manejo de cadenas en sort_helpers y fixstring

--- lab01/ej5/fixstring.c
+++ lab01/ej5/fixstring.c
@@ -5,15 +5,23 @@
 
 
 unsigned int fstring_length(fixstring s) {
-    /* copiá acá la implementación que hiciste en el ejercicio 0 */
+    int length = 0;
+    while(s[length] != '\0') length++;
+    return length;
 }
 
 bool fstring_eq(fixstring s1, fixstring s2) {
-    /* copiá acá la implementación que hiciste en el ejercicio 0 */
+    bool res = true;
+    unsigned int i = 0;
+    while(s1[i] != '\0' && s1[i] == s2[i]) i++;
+    return res = s1[i] == s2[i];
 }
 
 bool fstring_less_eq(fixstring s1, fixstring s2) {
-    /* copiá acá la implementación que hiciste en el ejercicio 0 */
+    bool res = true;
+    unsigned int i = 0;
+    while(s1[i] != '\0' && s1[i] == s2[i]) i++;
+    return res = s1[i] <= s2[i];
 }
 
 void fstring_set(fixstring s1, const fixstring s2) {
@@ -26,11 +34,10 @@ void fstring_set(fixstring s1, const fixstring s2) {
 }
 
 void fstring_swap(fixstring s1,  fixstring s2) {
-    fixstring aux;
-    /*
-     * COMPLETAR
-     *
-     */
+    fixstring aux = {'\0'};
+    fstring_set(aux, s1); // (aux,s1) fstring_set(s1, aux)
+    fstring_set(s1,s2);   // (s1,s2)  fstring_set(s2, s1)
+    fstring_set(s1, aux); // (s1,aux) fstring_set(aux, s1)
 }
 
 
