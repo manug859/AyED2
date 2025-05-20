78f4cb8782668c696542852064c406bd0e5dfd33
manug859 <manuel.agustin.gomez@mi.unc.edu.ar>
2025-05-20 01:03:17 -0300

    Agregar funciones de manipulación de cadenas: string_length, string_filter y string_is_symmetric

---
+++ lab04/lab04/ej4/b-checkpal/strfuncs.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include "strfuncs.h"
+
+size_t string_length(const char *str) {
+    size_t length = 0;
+    while(str[length] != '\0') {
+        length++;
+    }
+    return length;
+}
+
+char *string_filter(const char *str, char c) {
+
+   char *str_copy = NULL;                   
+   str_copy = malloc(string_length(str));   
+   
+   size_t i = 0;                            
+   size_t j = 0;                       
+
+   size_t length = string_length(str); 
+
+   while (i<length) {
+        if(str[i] != c) {
+            str_copy[j] = str[i];
+            j++;
+        }
+        i++;
+    }
+    str_copy[j] = '\0';
+    
+    str_copy = realloc(str_copy, j + 1); // +1 para el '\0'
+    
+    return str_copy;
+}
+
+bool string_is_symmetric(const char *str) {
+    size_t length = string_length(str);
+    bool res = true;
+    for(int i = 0, j = length - 1; i < j; i++, j--) {
+        res = str[i] == str[j] && res;
+    }
+    return res;
+}
---
+++ lab04/lab04/ej4/b-checkpal/strfuncs.h
@@ -0,0 +1,24 @@
+#ifndef  _STRFUNCS_H
+#define  _STRFUNCS_H
+
+#include <stdbool.h>
+
+size_t string_length(const char *str);
+/*
+    Calcula la longitud de la cadena apuntada por str.
+*/
+
+char *string_filter(const char *str, char c);
+/*
+    Devuelve una nueva cadena en memoria dinámica que
+    se obtiene tomando los caracteres de str que son
+    distintos del caracter c.
+*/
+bool string_is_symmetric(const char *str);
+/*
+    Indica si la cadena apuntada por str es simétrica
+    en cuanto que el primer caracter concide con el
+    último, el segundo con el penúltimo, etc.
+*/
+
+#endif
