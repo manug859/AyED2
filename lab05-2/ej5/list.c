3719eb05c3f3885b35ea7f4a93650bf12247bdcf
manug859 <manuel.agustin.gomez@mi.unc.edu.ar>
2025-06-11 19:23:55 -0300

    dxffdfd

--- lab05-2/ej5/list.c
+++ lab05-2/ej5/list.c
@@ -12,5 +12,79 @@ struct _list {
 };
 
 list empty() {
-    
+    list l = malloc(sizeof(struct _list));
+    l->size = 0;
+    return l;
 }
+
+list addl(elem e, list l) {
+    l->elems[(MAX_LENGTH - 1) - l->size] = e;
+    l->size += 1;
+    return l;  
+}
+
+bool is_empty(list l) {
+    return l->size == 0;
+}
+
+elem head(list l) {
+    assert(!is_empty(l));
+    return l->elems[MAX_LENGTH - 1];
+}
+
+list tail(list l) {
+    assert(!is_empty(l));
+    for(int i = MAX_LENGTH - 1; i > MAX_LENGTH - l->size; i--) {
+        l->elems[i] = l->elems[i-1]; 
+    }
+    l->size -= 1;
+    return l;
+}
+
+list addr(list l, elem e) {
+    for(int i = (MAX_LENGTH - 1) - l->size; i < MAX_LENGTH - 1; i++) {
+        l->elems[i-1] = l->elems[i]; 
+    }
+    l->elems[MAX_LENGTH-1] = e;
+    l->size += 1;
+    return l;
+}
+
+int length(list l) {
+    return l->size;
+}
+
+list concat(list l, list l0) {
+    assert(l->size + l0->size <= MAX_LENGTH);
+    for(int i = (MAX_LENGTH - 1) - l->size - l0->size; i < MAX_LENGTH - 1 - l0->size; i++) {
+        l->elems[i] = l->elems[i + l->size];
+    }
+    for(int j = (MAX_LENGTH - 1) - l0->size; j < MAX_LENGTH - 1; j++) {
+        l->elems[j] = l0->elems[j];
+    }
+    l->size = l->size + l0->size;
+    return l;
+}
+
+elem index(list l, int n) {
+    assert(n < length(l));
+    return l->elems[(MAX_LENGTH - 1) - n];
+}
+
+list take(list l, int n) {
+    if(n <= 0) {
+        l->size = 0;
+    }
+    else if (n < l->size) {
+        l->size = n;
+    }
+    return l;
+}
+
+
+
+
+
+/*
+
+*/
