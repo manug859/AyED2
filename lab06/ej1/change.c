8a5b564c09e591b8712d61d10512df136b2c9ae0
manug859 <manuel.agustin.gomez@mi.unc.edu.ar>
2025-06-18 17:52:30 -0300

    Refactor la función change para mejorar la legibilidad y corregir la lógica de inicialización de la tabla.

--- lab06/ej1/change.c
+++ lab06/ej1/change.c
@@ -31,7 +31,7 @@ int change(int d[], int n, int k) {
         for(int j = 0; j <= k; j++) {
             if(j == 0) {
                 table[i][j] = 0;
-            } else if (i == 0) {
+            } else if(i == 0) {
                 table[i][j] = INT_MAX;
             } else {
                 table[i][j] = 0;
@@ -40,17 +40,19 @@ int change(int d[], int n, int k) {
     }
 
     for(int i = 1; i <= n; i++) {
-        for(int j = 0; j <= k; j++) {
-            if(d[i-1] <= j && table[i][j - d[i - 1]] != INT_MAX) {
-                table[i][j] = MIN(table[i-1][j], table[i][j - d[i - 1]] + 1);
+        for(int j = 1; j <= k; j++) {
+            if(d[i-1] <= j && table[i][j - d[i-1]] != INT_MAX) {
+                table[i][j] = MIN(table[i-1][j], 1 + table[i][j - d[i-1]]);
             } else {
-                table[i][j] = table[i - 1][j];
+                table[i][j] = table[i-1][j];
             }
         }
     }
+
     printf("\n");
     print_table(n+1, k+1, table);
     printf("\n");
     printf("table[%d][%d] = %d\n", n, k, table[n][k]);
+    
     return table[n][k] == INT_MAX ? -1 : table[n][k];
 }
